3313bc344ac4015d199d42d0849e003fb097024b
SasiVatsal <71326149+sasivatsal7122@users.noreply.github.com>
2022-04-27 20:46:01 +0530

    -aw completed mid,weekly 27/4

--- dashboard.py
+++ dashboard.py
@@ -209,38 +209,154 @@ def Dashboard():
                     sem_2mid = main_data[username[0]]['sem2']
                     sem_3mid = main_data[username[0]]['sem3']
                     
-                    global sem_1_mid,sem_2_mid,sem_3_mid
-                    sem_1_mid = pd.DataFrame(sem_1mid)
+                    global sem_1_mid_df,sem_2_mid_df,sem_3_mid_df
+                    sem_1_mid_df = pd.DataFrame(sem_1mid)
                     
-                    sem_2_mid = pd.DataFrame(sem_2mid)
+                    sem_2_mid_df = pd.DataFrame(sem_2mid)
                     
-                    sem_3_mid = pd.DataFrame(sem_3mid)
+                    sem_3_mid_df = pd.DataFrame(sem_3mid)
+           
+            
+            def save_mid_data():
+                super_dict = {};superrr_dict ={}
+                for i in range(3):
+                    xxx = globals()[f'sem_{i+1}_mid_df']
+                    temporay_dict = xxx.to_dict()
+                    super_dict.update({f'sem{i+1}':temporay_dict})
+                superrr_dict.update({username[0]:super_dict})
+                with open(f'user_record/aids/{username[0]}/midterm.json', 'w') as fp:
+                    fp.seek(0)
+                    json.dump(superrr_dict,fp,indent = 4)
+            
+            mdict = {'m1':'Mid-Term-1','m2':'Mid-Term-2'}
+            def edit(df,sub,mid,marks):
+                i = df.index[df['Course Title']==sub].tolist()
+                try:
+                    df.at[i[0],mdict.get(mid)] = marks
+                    return df
+                except:
+                    return df
+      
+                    
             with st.expander("Semester - 1 Mid-term Record"):
-                st.dataframe(sem_1_mid)
+                sub_name = sem_1_mid_df.iloc[1]
+                sub,mid,marks = str(st.text_input("Enter Sub-Name, Mid - m1 or m2, marks : ",f"{sub_name['Course Title']},m1,18",key='midds-2')).split(',')
+                medit_btn = st.button("Edit Changes",key='mids-1')
+                if medit_btn:
+                    sem_1_mid_df = edit(sem_1_mid_df,sub,mid,marks)
+                    save_mid_data()
+                st.dataframe(sem_1_mid_df)
+            
             with st.expander("Semester - 2 Mid-term Record"):
-                st.dataframe(sem_2_mid)
+                sub_name = sem_2_mid_df.iloc[1]
+                sub,mid,marks = str(st.text_input("Enter Sub-Name, Mid - m1 or m2, marks : ",f"{sub_name['Course Title']},m1,18",key='midds-2')).split(',')
+                medit_btn = st.button("Edit Changes",key='mids-2')
+                if medit_btn:
+                    sem_2_mid_df = edit(sem_2_mid_df,sub,mid,marks)
+                    save_mid_data()
+                st.dataframe(sem_2_mid_df)
+            
             with st.expander("Semester - 3 Mid-term Record"):
-                st.dataframe(sem_3_mid)
+                sub_name = sem_3_mid_df.iloc[1]
+                sub,mid,marks = str(st.text_input("Enter Sub-Name, Mid - m1 or m2, marks : ",f"{sub_name['Course Title']},m1,18",key='midds-2')).split(',')
+                medit_btn = st.button("Edit Changes",key='mids-3')
+                if medit_btn:
+                    sem_3_mid_df = edit(sem_3_mid_df,sub,mid,marks)
+                    save_mid_data()
+                st.dataframe(sem_3_mid_df)
+            
+            final_mid_save = st.button("Save My Data in Database",key='finalmidsave')
+            if final_mid_save:
+                super_dict = {};superrr_dict ={}
+                for i in range(3):
+                    xxx = globals()[f'sem_{i+1}_mid_df']
+                    temporay_dict = xxx.to_dict()
+                    super_dict.update({f'sem{i+1}':temporay_dict})
+                superrr_dict.update({username[0]:super_dict})
+                with open(f'user_record/aids/{username[0]}/midterm.json', 'w') as fp:
+                    fp.seek(0)
+                    json.dump(superrr_dict,fp,indent = 4)
+                st.success("Your Data Has Been Successufully updated in the DataBase")
+                
+                
         
         elif user_option=='Weekly':
+            st.info(f"Hey {name[0]} we suggest wide mode for this specific section for better usability and visibility")
             with open(f'user_record/aids/{username[0]}/weekly.json') as d:
                     main_data = json.load(d)
                     sem_1weekly = main_data[username[0]]['sem1']
                     sem_2weekly = main_data[username[0]]['sem2']
                     sem_3weekly = main_data[username[0]]['sem3']
                     
-                    global sem_1_weekly,sem_2_weekly,sem_3_weekly
-                    sem_1_weekly = pd.DataFrame(sem_1weekly)
+                    global sem_1_weekly_df,sem_2_weekly_df,sem_3_weekly_df
+                    sem_1_weekly_df = pd.DataFrame(sem_1weekly)
 
-                    sem_2_weekly = pd.DataFrame(sem_2weekly)
+                    sem_2_weekly_df = pd.DataFrame(sem_2weekly)
                     
-                    sem_3_weekly = pd.DataFrame(sem_3weekly)
+                    sem_3_weekly_df = pd.DataFrame(sem_3weekly)
+                    
+                
+            def save_weekly_data():
+                super_dict = {};superrr_dict ={}
+                for i in range(3):
+                    xxx = globals()[f'sem_{i+1}_weekly_df']
+                    temporay_dict = xxx.to_dict()
+                    super_dict.update({f'sem{i+1}':temporay_dict})
+                superrr_dict.update({username[0]:super_dict})
+                with open(f'user_record/aids/{username[0]}/weekly.json', 'w') as fp:
+                    fp.seek(0)
+                    json.dump(superrr_dict,fp,indent = 4)
+            
+            wdict = {'w1':'Weekly-1','w2':'Weekly-2','w3':'Weekly-3','w4':'Weekly-4','w5':'Weekly-5','w6':'Weekly-6'}
+            def edit(df,sub,mid,marks):
+                i = df.index[df['Course Title']==sub].tolist()
+                try:
+                    df.at[i[0],wdict.get(mid)] = marks
+                    return df
+                except:
+                    return df
+            
             with st.expander("Semester - 1 Weekly Test Record"):
-                st.dataframe(sem_1_weekly)
+                sub_name = sem_1_weekly_df.iloc[1]
+                sub,mid,marks = str(st.text_input("Enter Sub-Name, Weekly - w1 or w2 or...w6, marks : ",f"{sub_name['Course Title']},w1,18",key='weekly-1')).split(',')
+                wedit_btn = st.button("Edit Changes",key='weekly-1')
+                if wedit_btn:
+                    sem_1_weekly_df = edit(sem_1_weekly_df,sub,mid,marks)
+                    save_weekly_data()
+                st.dataframe(sem_1_weekly_df)
+
             with st.expander("Semester - 2 Weekly Test Record"):
-                st.dataframe(sem_2_weekly)
+                sub_name = sem_2_weekly_df.iloc[1]
+                sub,mid,marks = str(st.text_input("Enter Sub-Name, Weekly - w1 or w2 or...w6, marks : ",f"{sub_name['Course Title']},w1,18",key='weekly-2')).split(',')
+                wedit_btn = st.button("Edit Changes",key='weekly-2')
+                if wedit_btn:
+                    sem_2_weekly_df = edit(sem_2_weekly_df,sub,mid,marks)
+                    save_weekly_data()
+                st.dataframe(sem_2_weekly_df)
+
             with st.expander("Semester - 3 Weekly Test Record"):
-                st.dataframe(sem_3_weekly)
+                sub_name = sem_3_weekly_df.iloc[1]
+                sub,mid,marks = str(st.text_input("Enter Sub-Name, Weekly - w1 or w2 or...w6, marks : ",f"{sub_name['Course Title']},w1,18",key='weekly-3')).split(',')
+                wedit_btn = st.button("Edit Changes",key='weekly-3')
+                if wedit_btn:
+                    sem_3_weekly_df = edit(sem_3_weekly_df,sub,mid,marks)
+                    save_weekly_data()
+                st.dataframe(sem_3_weekly_df)
+                
+            final_weekly_save = st.button("Save My Data in Database",key='finalweeklysave')
+            if final_weekly_save:
+                super_dict = {};superrr_dict ={}
+                for i in range(3):
+                    xxx = globals()[f'sem_{i+1}_weekly_df']
+                    temporay_dict = xxx.to_dict()
+                    super_dict.update({f'sem{i+1}':temporay_dict})
+                superrr_dict.update({username[0]:super_dict})
+                with open(f'user_record/aids/{username[0]}/weekly.json', 'w') as fp:
+                    fp.seek(0)
+                    json.dump(superrr_dict,fp,indent = 4)
+                st.success("Your Data Has Been Successufully updated in the DataBase")
+                
+
                     
 
     elif user_option=='Analytics':
